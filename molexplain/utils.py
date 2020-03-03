20c1b1d74d93418a98a4e1926e447ce9a6b2c386
josejimenezluna <hawk31@gmail.com>
2020-03-03 17:24:57 +0100

    project start

---
+++ molexplain/utils.py
@@ -0,0 +1,6 @@
+import os
+
+
+DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
+RAW_DATA_PATH = os.path.join(os.path.dirname(__file__), 'data', 'raw')
+PROCESSED_DATA_PATH = os.path.join(os.path.dirname(__file__), 'data', 'processed')
