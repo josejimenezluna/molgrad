5380c7762c0301cdfb7eaa30ed2fa4aa65af98e8
José Jiménez <hawk31@gmail.com>
2020-08-28 13:27:10 +0200

    noise tunnel prototype

--- molexplain/ig.py
+++ molexplain/ig.py
@@ -35,10 +35,57 @@ def gen_steps(graph, g_feat, n_steps, version=2):
     return graphs, g_feats
 
 
-def integrated_gradients(graph, g_feat, model, task, n_steps=50, version=2):
+def noise_tunnel(graph, g_feat, n_samples=10, version=2, mean=0.0, std=1.0):
+    """Noise tunnel prototype
+
+    Parameters
+    ----------
+    graph : DGL graph
+    g_feat : torch.Tensor
+    n_samples : int, optional
+        number of samples to add noise to, by default 10
+    version : int, optional
+    mean : float, optional
+    std : float, optional
+
+    Returns
+    -------
+    list
+        A list of graphs with their respective node and edge
+        features with noise added to them.
     """
-    Computes path integral of the node features of `graph` for a
+    graphs = []
+
+    feat = graph.ndata['feat']
+    e_feat = graph.edata['feat']
+
+    for _ in range(n_samples):
+        g = deepcopy(graph)
+        g.ndata['feat'] = feat + (mean + std * torch.randn(feat.shape))
+        if version == 3:
+            g.edata['feat'] = e_feat + (mean + std * torch.randn(e_feat.shape))
+        graphs.append(g)
+    return graphs
+
+
+def integrated_gradients(graph, g_feat, model, task=0, n_steps=50, version=2):
+    """Computes path integral of the node features of `graph` for a
     specific `task` number, using a Monte Carlo approx. of `n_steps`. 
+
+    Parameters
+    ----------
+    graph : DGL graph
+    g_feat : torch.Tensor
+    model : MPNN 
+    task : int, optional
+    n_steps : int, optional
+    version : int, optional
+
+    Returns
+    -------
+    atom_importances : torch.Tensor
+    bond_importances : torch.Tensor
+    values_global : torch.Tensor
     """
     graphs, g_feats = gen_steps(graph, g_feat, n_steps=n_steps, version=version)
     values_atom = []
